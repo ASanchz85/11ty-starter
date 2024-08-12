e610040d0c76146480ec498e81dacbaef2386eb8
ASanchz85 <asanchz85@gmail.com>
2024-08-12 14:37:04 +0200

    FEAT: general layout for main pages done

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/assets')
+  eleventyConfig.addPassthroughCopy('src/css')
+  eleventyConfig.addPassthroughCopy('src/js')
+  
   return {
     dir: {
       input: 'src',
